bc450f881437fc88b470a874da06dea38bc3f872
tureraosantosh <tureraosantosh@gmail.com>
2025-09-02 11:17:25 +0530

    first session commited

---
+++ Session-01-VariablesDataType.js
@@ -0,0 +1,24 @@
+var age=40;//number
+var name="Vijay"; //string 
+var  arePassedSSC=true; // boolean
+var   aadhar=null;
+var email;
+// console.log("email :: "+email);
+// console.log("age "+age);
+// console.log("Name :: "+name);
+// console.log("Passed "+arePassedSSC)
+// console.log("aadhar "+aadhar);
+//Non-primitive 
+//Object 
+
+var student=
+{
+"name":"Vijay",
+"subject":"Java",
+"email":"s@gmail.com"
+}
+
+console.log(student.name)
+student.email="google@googley.com"
+console.log(student.email)
+
