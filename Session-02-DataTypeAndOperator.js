5b7bec587e29dfd759ff057a327280d0dbe2f621
aaplazoom <aaplazoom@gmail.com>
2025-09-03 21:36:26 +0530

    session2 uploaded

---
+++ Session-02-DataTypeAndOperator.js
@@ -0,0 +1,9 @@
+// var fruits=["Apple","Banana"];
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+
+//Multidimension
+
+var  jimmiTower=[[101,102,103,104],[201,202,203,204]];
+console.log(jimmiTower[1][3]);
+
