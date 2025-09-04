9c14abd6af9dc2fecb487693474e5d6ef530b20c
aaplazoom <aaplazoom@gmail.com>
2025-09-04 08:34:56 +0530

    session 03 added

---
+++ Session-03-Operators.js
@@ -0,0 +1,53 @@
+//Arithmatic operator +-/*%
+
+var num1=10;
+var num2=30;
+var total=num1/num2;
+// console.log(total)
+
+//Assigment Operator 
+var age=28;
+
+var num2=20;
+var num3=10;
+var sum=num2;
+// console.log(sum)
+
+//comparison 
+var result=10<20;
+var age=20;
+var ageResult=20!=20;
+// console.log(ageResult)
+
+//logical 
+//  && , ||  
+ var userid=123;
+ var password=321;
+//and example all the condition should true then only return true 
+//false
+ var login=userid==123&&password==321;
+//  console.log(login) 
+
+ //or single true will return true 
+
+ var mobile=123456786;
+ var email='sagar@aditya.com';
+
+ var result=mobile==1234567891||email=='sagar@aditya.com';
+ console.log(result)
+
+
+//  3.Tenernary Operaator
+  var marks=40;
+  var passsing=marks>35?"You are passed":"You are failed";
+  console.log(passsing);
+
+//   var age=21;
+
+//   var resultAge=age>=12?"You are eligible for voting":"You are not eligible for Voting";
+//   console(resultAge)
+
+  var cash=40;
+
+  var cashResult=cash>30?"Burger":"Vadapav";
+  console.log(cashResult)
