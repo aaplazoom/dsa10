9d74719299696a334821401b1c7b9048410dac0d
aaplazoom <aaplazoom@gmail.com>
2025-09-08 08:28:35 +0530

    Session 5 uploaded

---
+++ Session-04-Statements.js
@@ -0,0 +1,57 @@
+// var marks=36;
+// //85 A
+// // 65 B
+// //45 // C
+// //35 passing 
+// // whenever condition will satisfy then else if block will not chec next condtion
+// console.log(marks>36)
+// if(marks>85)
+// {
+//    console.log("A Grade") 
+// }
+// else if(marks>65)
+// {
+//  console.log("B Grade ")
+// }
+// else if(marks>45)
+// {
+//     console.log("Pass C ")
+// }
+// else if(marks>35)
+// {
+//  console.log("Pass")
+// }
+// else {
+//     console.log("You are enjoy 1 more year in same class with new frnds")
+// }
+
+// for(var i=1;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+//reverse loop
+// for(var i=10;i>0;i--)
+// {
+//     console.log(i);
+// }
+
+// condition true or false 
+// var i=11;
+//  while(i<=10)
+//  {
+//     console.log(i);
+//  i++;
+//  }
+
+// var i=1;
+// do
+// {
+//  console.log(i);
+//  i++;
+// }while(i<=10)
+
+for(var j=20;j>=20;j--)
+{
+    console.log(j)
+}
---
+++ Session-05-Functions.js
@@ -0,0 +1,44 @@
+// var num1=10;
+// var num2=20;
+// var total=num1+num2;
+
+// var num2=50;
+// var num3=60;
+// var total2=num2+num3;
+
+// console.log(total)
+// console.log(total2)
+
+// function addition(num1,num2)
+// {
+
+//     total=num1+num2;
+//     console.log("Addtion ::"+total)
+// }
+// function substraction(num1,num2)
+// {
+//  total=num1-num2;
+//  console.log("sub "+total)
+// }
+
+// addition(10,20);
+// addition(40,50);
+// addition(70,90);
+// substraction(100,50);
+// substraction(100,65);
+
+
+// function add(username){
+
+//     console.log("Welcome "+username)
+
+// }
+// add("Rahul");
+
+function mult(num1,num2)
+{
+  return num1*num2;
+}
+
+var output=mult(5,4);
+console.log(output);
